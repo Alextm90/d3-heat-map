9f0e3b7af272cb789331082736f9e0bc790dd4b0
Alex Miserandino <amiserandino20@gmail.com>
2023-05-30 23:46:43 -0700

    worked on graph

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const padding = 100
 const width = 1400
-const height = 700
+const height = 900
 const tickMonths = ['','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] 
+const colorArray = ["red", "blue", "orange", "grey", "pink", "green", "black", "purple", "yellow", "aqua", "white" ] 
+const tempArray = [1.6, 2.7, 3.8, 4.9, 6, 7.2, 8.3, 9.4, 10.5, 11.6, 12.7, 13.9]
 
 // create svg + title
 const svg = d3.select('body')
@@ -24,7 +26,7 @@ const map = async () => {
 
 // create x-axis + scale
 const xScale = d3.scaleTime()
-       .domain([new Date(d3.min(data.monthlyVariance, (d) => d.year)), new Date(d3.max(data.monthlyVariance, (d) => d.year + 1))])
+       .domain([d3.min(data.monthlyVariance, (d) => d.year), d3.max(data.monthlyVariance, (d) => d.year + 1)])
        .range([padding, width - padding])
     
 const xAxis = d3.axisBottom()
@@ -35,40 +37,78 @@ const xAxis = d3.axisBottom()
 svg.append('g')
         .call(xAxis) 
         .attr('id', 'x-axis')
-        .attr("transform", 'translate(0,  ' + (height - padding) + ')')
+        .attr("transform", 'translate(0,  ' + (height - (padding * 3)) + ')')
 
 // create y-axis + scale
-const yScale = d3.scaleLinear()
-       .domain([12.5, 0.5])
-       .range([height/1.4, 0])
+const yScale = d3.scaleBand()
+       .domain([12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+       .range([height - padding * 4, 0])
     
 const yAxis = d3.axisLeft()
         .scale(yScale)
         .tickFormat((d,i) => { 
             return tickMonths[d] // first "d" = 12
         }) 
-console.log(data.monthlyVariance.length)
+
 svg.append('g')
         .call(yAxis) 
         .attr('id', 'y-axis')
         .attr('transform', 'translate(' + padding + ', ' + padding + ')')
 
-// add data points
+// create legend
+const legendScale = d3.scaleBand()
+        .domain(tempArray) 
+        .range([0, 350])
 
-svg.selectAll('rect')
+const legendAxis = d3.axisBottom()
+        .scale(legendScale)
+        .ticks(9)
+        
+const legend = svg.append('g')
+        .attr('id', 'legend')
+        .call(legendAxis) 
+        .attr('transform', 'translate(' + 300 + ', ' + 675 + ')')
+
+ legend.selectAll('rect')
+        .data(tempArray)
+        .enter()
+        .append('rect')
+        .attr('class', 'rect')
+        .attr('y', padding)
+        .attr('x', (d, i) => {
+            console.log(d)
+            return legendScale(d) 
+        })
+        .attr('fill', 'red')
+        .attr('width', 40) 
+        .attr('height', 200)
+        .attr('border', '1px solid black')
+        
+
+// add data points
+svg.selectAll('.cell')
         .data(data.monthlyVariance)
         .join('rect')
         .attr('x', (d, i) => {
-            return xScale(new Date(d.year)) 
+            return xScale(d.year) 
           })
         .attr('y', (d, i) => {
             return yScale(d.month)  
        })
+        .attr('width', 1200/(data.monthlyVariance.length/12)) 
+        .attr('height', (height - padding * 4) / 12)
         .attr("class", "cell")
-        .attr('width', 1200/(data.monthlyVariance.length/12))
-        .attr('height', (height - padding * 2) / 12)
-        .attr('fill', 'red')
-        .attr("transform", "translate(0," + 79 + ")")
+        .attr('fill', (d, i) => {
+            for (let n = 0; n < tempArray.length; n++) {
+            if (data.baseTemperature + d.variance >= tempArray[n] && data.baseTemperature + d.variance < tempArray[n + 1]) {
+                return colorArray[n]
+            }
+            }
+        })
+        .attr("transform", "translate(0," + padding + ")")
+        .attr("data-month", (d) => d.month - 1)
+        .attr("data-year", (d) => d.year)
+        .attr("data-temp", (d) => d.variance)
      })
 }
 map()
