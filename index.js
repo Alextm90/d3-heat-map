ec8c8257dbcb81f7d1e17938916931ba52266052
Alex Miserandino <amiserandino20@gmail.com>
2023-05-31 23:02:01 -0700

    finished design

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const padding = 100
 const width = 1400
-const height = 900
-const tickMonths = ['','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] 
-const colorArray = ["red", "blue", "orange", "grey", "pink", "green", "black", "purple", "yellow", "aqua", "gold" ] 
+const height = 700
+const round = d3.format(".1f")
+const monthArray = ['','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] 
+const colorArray = ["#004A98", "#0071CE", "#86bce8", "#b6d7eb", "#efe57a", "#ffc846", "#ffa300", "#ff7f00", "#fe4811", "#df231d", "#96262c" ] 
 const tempArray = [1.6, 2.7, 3.8, 4.9, 6, 7.2, 8.3, 9.4, 10.5, 11.6, 12.7, 13.9]
 
 // create svg + title
@@ -10,13 +11,23 @@ const svg = d3.select('body')
         .append('svg')
         .attr('width', width)
         .attr('height', height)
+        .attr("transform", 'translate(0, 150)')
 
 svg.append('text')
-        .text('D3 heat map')
+        .text(`Monthly Global Land-Surface Temperature`)
         .attr('id', 'description')
-        .attr('x', width/2)
-        .attr('y', padding)
+        .attr('x', 490)
+        .attr('y', 80)
         .attr('font-size', '36px')
+        .attr('transform', 'translate(-100, -50)')
+
+svg.append('text')
+        .text(`1753 - 2015: Base Temperature 8.66℃`)
+        .attr('id', 'description')
+        .attr('x', 510)
+        .attr('y', 80)
+        .attr('font-size', '26px')
+        .attr('transform', 'translate(-10, -10)')
 
 // fetch data
 const map = async () => {
@@ -26,8 +37,8 @@ const map = async () => {
 
 // create x-axis + scale
 const xScale = d3.scaleTime()
-       .domain([d3.min(data.monthlyVariance, (d) => d.year), d3.max(data.monthlyVariance, (d) => d.year + 1)])
-       .range([padding, width - padding])
+        .domain([d3.min(data.monthlyVariance, (d) => d.year), d3.max(data.monthlyVariance, (d) => d.year + 1)])
+        .range([padding, width - padding])
     
 const xAxis = d3.axisBottom()
         .scale(xScale)
@@ -37,17 +48,17 @@ const xAxis = d3.axisBottom()
 svg.append('g')
         .call(xAxis) 
         .attr('id', 'x-axis')
-        .attr("transform", 'translate(0,  ' + (height - (padding * 3)) + ')')
+        .attr("transform", 'translate(0,  ' + (height - (padding * 2)) + ')')
 
 // create y-axis + scale
 const yScale = d3.scaleBand()
-       .domain([12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
-       .range([height - padding * 4, 0])
+        .domain([12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+        .range([height - padding * 3, 0])
     
 const yAxis = d3.axisLeft()
         .scale(yScale)
         .tickFormat((d,i) => { 
-            return tickMonths[d] // first "d" = 12
+            return monthArray[d] 
         }) 
 
 svg.append('g')
@@ -58,7 +69,7 @@ svg.append('g')
 // create legend
 const legendScale = d3.scaleBand()
         .domain(tempArray) 
-        .range([0, 300])
+        .range([0, 400])
 
 const legendAxis = d3.axisBottom()
         .scale(legendScale)
@@ -66,7 +77,7 @@ const legendAxis = d3.axisBottom()
 const legend = svg.append('g')
         .attr('id', 'legend')
         .call(legendAxis) 
-        .attr('transform', 'translate(' + 300 + ', ' + 675 + ')')
+        .attr('transform', 'translate(' + padding + ', 600)')
 
  legend.selectAll('.rect')
         .data(tempArray)
@@ -80,14 +91,19 @@ const legend = svg.append('g')
         .attr('fill', (d, i) => {
                     return colorArray[i] 
         })
-        .attr('width', 24.36) 
+        .attr('width', 33) 
         .attr('height', 30)
         .style("outline", (d, i) => {
             if (i != 11) return "1px solid black"
         })  
-       .attr("transform", 'translate(13,  ' + - 130 + ')')
+       .attr("transform", 'translate(17,' + -131 + ')')
 
-// add data points
+// create tooltip
+const tooltip = d3.select('body')
+        .append('div')
+        .attr('id', 'tooltip')
+
+// add data points to chart
 svg.selectAll('.cell')
         .data(data.monthlyVariance)
         .join('rect')
@@ -98,7 +114,7 @@ svg.selectAll('.cell')
             return yScale(d.month)  
        })
         .attr('width', 1200/(data.monthlyVariance.length/12)) 
-        .attr('height', (height - padding * 4) / 12)
+        .attr('height', (height - padding * 3) / 12)
         .attr("class", "cell")
         .attr('fill', (d, i) => {
             for (let n = 0; n < tempArray.length; n++) {
@@ -111,7 +127,20 @@ svg.selectAll('.cell')
         .attr("data-month", (d) => d.month - 1)
         .attr("data-year", (d) => d.year)
         .attr("data-temp", (d) => d.variance)
-
+        .on('mouseover', (event, d) => { 
+          tooltip
+            .html(` ${monthArray[d.month]} ${d.year} <br> Temp: ${round(data.baseTemperature + d.variance) + "°C"} <br> Variance: ${round(d.variance) + "°C"}`)
+            .style('opacity', .9)
+            .style('position', 'absolute')
+            .style('left', (event.pageX + 25) + 'px')
+            .style('top', (event.pageY - 55) + 'px')
+            .style('background-color', '#75c6e3')
+            .attr('data-year', d.year)
+       })
+        .on('mouseout', () => {
+          tooltip
+            .style("opacity", 0)
+          })
      })
 }
 map()
